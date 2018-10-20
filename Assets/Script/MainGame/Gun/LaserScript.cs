f91ba598b25dae9c1069333ff3342c66da9d0e1e
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-10-20 09:47:47 +0900

    レーザー実装

--- Assets/Script/MainGame/Gun/LaserScript.cs
+++ Assets/Script/MainGame/Gun/LaserScript.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserScript : MonoBehaviour {
+public class LaserScript : MonoBehaviour
+{
 
-	float m_Force = 500.0f;
+	float m_Force = 1000.0f;
 	float m_TimeCnt;
 	public GameObject m_EffectPrefab;
 	GameObject m_Effect;
-	
+	public GameObject m_ExpPrefab;
+	public GameObject m_AudioPrefab;
+	GameObject[] m_Exp;
+	GameObject m_GunSE;
+
+	const int m_MaxExpNum = 10;
+	int m_Num;
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		m_TimeCnt += Time.deltaTime;
 		if (m_TimeCnt > 1.5f)
 		{
-			Destroy(m_Effect);
 			Destroy(gameObject);
+		}
+		if (m_TimeCnt > 0.4f)
+		{
+			Destroy(m_Effect);
 		}
 	}
 
@@ -26,8 +37,24 @@ public class LaserScript : MonoBehaviour {
 		m_Effect = Instantiate(m_EffectPrefab);
 		m_Effect.transform.localRotation = gunTransform.transform.localRotation;
 		m_Effect.transform.position = gunTransform.transform.position;
+		m_Num = 0;
+		m_GunSE = Instantiate(m_AudioPrefab);
+		m_GunSE.GetComponent<AudioSource>().Play();
 
 		m_TimeCnt = 0.0f;
 		GetComponent<Rigidbody>().AddForce(transform.forward * m_Force);
+
+		m_Exp = new GameObject[m_MaxExpNum];
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (m_Num < m_MaxExpNum)
+		{
+			m_Exp[m_Num] = Instantiate(m_ExpPrefab);
+			m_Exp[m_Num].transform.position = other.gameObject.transform.position;
+			Destroy(m_Exp[m_Num], 1.5f);
+			m_Num++;
+		}
 	}
 }
