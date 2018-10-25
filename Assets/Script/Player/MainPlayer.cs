5c41abd92586b4069f8029249e890e025bbbc34a
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-10-25 19:45:55 +0900

    なんかプレイヤーのやつ

---
+++ Assets/Script/Player/MainPlayer.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+
+	public float m_TransTimeSec = 0.2f;     // 武器変形の際の移動速度
+	GameObject[] m_EnemyObjs;       // 0 = 左手、1 = 右手
+	Vector3 m_OriginPos; // プレイヤーの基本座標
+	Vector3 m_MovePos;   // 移動する方向の座標
+	Vector3 m_LeavePos; // 移動元座標
+
+	GameObject m_CameraObj; // プレイヤーの視点となるカメラオブジェクト
+	float m_TransCnt;
+
+	public enum HandData
+	{
+		Right,
+		Left
+	}
+
+	enum PlayerTrans
+	{
+		Attack,     // 攻撃中
+		Translate   // 移動中
+	}
+
+	PlayerTrans m_Trans;
+
+	// Use this for initialization
+	void Start()
+	{
+		m_TransCnt = 0;
+		m_CameraObj = GameObject.Find("Camera (eye)");
+		
+		m_OriginPos = m_CameraObj.transform.position;
+		m_LeavePos = m_OriginPos;
+		m_EnemyObjs = new GameObject[2];
+		m_EnemyObjs[0] = gameObject;
+		m_EnemyObjs[1] = gameObject;
+
+		m_Trans = PlayerTrans.Attack;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		switch (m_Trans)
+		{
+			case PlayerTrans.Attack:
+				break;
+			case PlayerTrans.Translate:
+				m_CameraObj.transform.position = Vector3.zero;
+				Translate();
+				break;
+		}
+	}
+
+		/// <summary>
+		/// 弾が当たった敵の情報をプレイヤーに渡す関数
+		/// </summary>
+		/// <param name="hand"></param>
+		/// <param name="obj"></param>
+		public void SetEnemyObj(HandData hand, GameObject obj)
+	{
+		switch (hand)
+		{
+			case HandData.Right:
+				m_EnemyObjs[0] = obj;
+				break;
+			case HandData.Left:
+				m_EnemyObjs[1] = obj;
+				break;
+		}
+	}
+
+	/// <summary>
+	///  敵の近づく際の命令
+	/// </summary>
+	public void Proximity(HandData hand)
+	{
+		switch (hand)
+		{
+			case HandData.Right:
+				m_MovePos = m_EnemyObjs[0].transform.position;
+				break;
+			case HandData.Left:
+				m_MovePos = m_EnemyObjs[1].transform.position;
+				break;
+		}
+
+		m_LeavePos = gameObject.transform.position;
+		m_Trans = PlayerTrans.Translate;
+	}
+
+	/// <summary>
+	/// 敵から離れる時の命令
+	/// </summary>
+	public void LongDistance()
+	{
+		m_LeavePos = gameObject.transform.position;
+		m_MovePos = m_OriginPos;
+		m_Trans = PlayerTrans.Translate;
+	}
+
+	// 移動中処理
+	void Translate()
+	{
+		// 移動処理
+		float t = m_TransCnt / m_TransTimeSec;
+		gameObject.transform.position = Vector3.Lerp(m_LeavePos, m_MovePos,t);
+		m_TransCnt += Time.deltaTime;
+
+		// 移動が終わった
+		if(m_TransCnt > m_TransTimeSec)
+		{
+			m_TransCnt = 0.0f;
+			//m_CameraObj.transform.position = m_MovePos;
+			m_Trans = PlayerTrans.Attack;
+		}
+	}
+}
