9b9ba23b781f4af1157fed8763035fe5b0de3bb1
yunosuke matsumoto <yunosukematsumoto.hal@gmail.com>
2019-01-30 04:53:43 +0900

    BGMと効果音付けた

--- Assets/Script/UI/SceneTrans.cs
+++ Assets/Script/UI/SceneTrans.cs
@@ -6,17 +6,21 @@ public class SceneTrans : MonoBehaviour
 {
 	public string m_NextScene;
 	public FadeScript m_Fade;
+	public AudioSource m_Audio;
 
     // Update is called once per frame
     void Update()
     {
-		if (ViveCtrl.Get.Trigger(ViveCtrl.ViveDeviceType.RightHand,ViveCtrl.ViveKey.Trigger))
-		{
-			m_Fade.m_FadeInFlag = false;
-		}
-
+		//if (ViveCtrl.Get.Trigger(ViveCtrl.ViveDeviceType.RightHand,ViveCtrl.ViveKey.Trigger))
+		//{
+		//	m_Fade.ChangeFlag(false);
+		//	m_Audio.Play();
+		//}
 		if (Input.GetKeyDown(KeyCode.Space))
+		{
 			m_Fade.ChangeFlag(false);
+			m_Audio.Play();
+		}
 
 		if (m_Fade.SceneTrans())
 		{
